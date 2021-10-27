c748fc0ecad1d542993735f91757e341705ed3d0
Polina <54813973+fokicheva@users.noreply.github.com>
2021-10-27 16:07:19 +0100

    process text + create word cloud object
    - format code
    - import/install necessary libs
    - process tweet text
    - create word cloud object

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import tweepy
+import json
+import re
+import string
+import emoji
+from bs4 import BeautifulSoup
+import nltk
+import operator
+token = nltk.WordPunctTokenizer()
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
 
 # TODO:
 # normalise data for word cloud
@@ -17,6 +27,8 @@ API_SECRET = ""
 # change path to where your API key .txt is stored, with 1st line being public key and 2nd line being private key
 API_PATH = "../API_keys.txt"
 
+
+# ----- TWITTER ----- #
 def read_keys(path):
     global API_KEY
     global API_SECRET
@@ -61,6 +73,11 @@ def get_tweets(api, name):
         print(e)
         return {}
 
+# ----- END TWITTER ----- #
+
+
+
+# ----- TWEET PROCESSING ----- #
 
 def get_master_string(tweets):
     master_string = ""
@@ -85,10 +102,71 @@ def check_full_text(tweet):
         text = tweet["extended_tweet"]["full_text"]
     return text
 
+# ----- END TWEET PROCESSING ----- #
+
+
+
+# ----- WORDCLOUD ----- #
+
+def create_wordcloud_object(master_string):
+    prepared_text = prepare_text(master_string)
+    nouns_text = get_nouns(prepared_text)
+    stopwords = set(STOPWORDS)
+    stopwords.update(get_data_from_file("word_lists/curse_words.json"))
+    stopwords.update(get_data_from_file("word_lists/custom_words.json"))
+    wordcloud = WordCloud(width=1400, stopwords=stopwords,height=1000,max_font_size=200,max_words=80,collocations=False, background_color='white').generate(nouns_text)
+    return wordcloud
+
+
+def prepare_text(text):
+    # using this article for data cleaning https://medium.com/codex/making-wordcloud-of-tweets-using-python-ca114b7a4ef4
+    # remove html characters
+    del_amp = BeautifulSoup(text, 'lxml')
+    del_amp_text = del_amp.get_text()
+    # remove mentions and hashes
+    re_list = ["@[A-Za-z0–9_]+", "#"]
+    combined_re = re.compile( '|'.join( re_list) )
+    del_link_mentions = re.sub(combined_re, '', del_amp_text)
+    # remove emojis
+    del_emoji = emoji.get_emoji_regexp().sub("", del_link_mentions)
+    # # remove links
+    re_links = re.compile(r'(https?:\/\/)?(www\.)?(\w+\.)?(\w+)(\.\w+)(\/\S+)?')
+    del_links = re.sub(re_links, "", del_emoji)
+    # make lower case
+    lower_case = del_links.lower()
+    # tokenize
+    words = token.tokenize(lower_case)
+    # get rid of words below length 2
+    result_words = [x for x in words if (len(x) > 2) and (len(x) < 15)]
+    # return (" ".join(result_words)).strip()
+    return result_words
+
+
+def get_nouns(words):
+    tagged_text = nltk.pos_tag(words)
+    nouns_text = []
+    for word in tagged_text:
+        if word[1] == "NN":
+            nouns_text.append(word[0])
+    return (" ".join(nouns_text)).strip()
+
+# ----- END WORDCLOUD ----- #
+
+
+# ----- GENERAL METHODS ----- #
+
+def get_data_from_file(filename):
+    with open(filename) as f_in:
+        data = json.loads(f_in.read())
+        return data
+
+
+# ----- END GENERAL METHODS ----- #
 
 
 if __name__ == "__main__":
     read_keys(API_PATH)
     api = get_api()
     tweets = get_tweets(api, "PFokicheva")
-    print(get_master_string(tweets))
+    master_string = get_master_string(tweets)
+    wordcloud = create_wordcloud_object(master_string)
